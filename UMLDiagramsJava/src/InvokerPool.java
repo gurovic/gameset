fb2c30fffe415affc4ee9f5d883a7a13f60d51a1
seawind777 <SA_gaponov@bk.ru>
2023-03-01 08:56:47 +0300

    Redacted InvokerPool class

--- UMLDiagramsJava/src/InvokerPool.java
+++ UMLDiagramsJava/src/InvokerPool.java
@@ -2,7 +2,8 @@ public class InvokerPool {
     private int max_invokers;
     private int busy_invokers;
     private List<InvokerRequest> request_queue;
-    private void wait_for_free_invokers(){};
+    private void waitForFreeSpace(){};
     private void runInvokers(InvokerRequest request){}
+    private void freeInvokerSpace(){}
 
 }
