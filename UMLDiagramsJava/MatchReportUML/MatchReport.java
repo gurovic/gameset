e76828477ae99dfa9df7d1f93d0afd89075925ce
username123654 <ustinov.arseny.07@gmail.com>
2023-03-05 18:46:42 +0300

    added java classes for uml

---
+++ UMLDiagramsJava/MatchReportUML/MatchReport.java
@@ -0,0 +1,47 @@
+import java.util.Map;
+
+public class MatchReport {
+
+    private long matchId;
+    private Map[long, InvokerReport] invokerReports;
+
+    private Map[long, int] solutionScores;
+    private Map[long, int] solutionPlaces;
+
+    private boolean placesOnly;
+
+    public MatchReport (private long matchId, Map[long, InvokerReport] invokerReports,
+                        Map[long, int] solutionScores, Map[long, int] solutionPlaces, boolean placesOnly) {
+        this.matchId = matchId;
+        this.invokerReports = invokerReports;
+        this.solutionScores = solutionScores;
+        this.solutionPlaces = solutionPlaces;
+        this.placesOnly = placesOnly;
+    } /* just in case; may be not needed or changed */
+
+    public void getMatchId() {
+        return matchId;
+    }
+
+    public void getPlacesOnly() {
+        return placesOnly;
+    }
+
+    public InvokerReport getSolutionReport(long solutionId) {
+        return invokerReports[solutionId];
+    }
+
+    public int getSolutionScore(long solutionId) {
+        return solutionScores[solutionId];
+    }
+
+    public int getSolutionPlace(long solutionId) {
+        return solutionPlaces[solutionId];
+    }
+
+    public void submitToTournamentSystem(Tournament tournament) {
+        /* Submit the report; placeholder, may be executed by Match instead */
+        System.out.println("Report submitted")
+        return;
+    }
+}
