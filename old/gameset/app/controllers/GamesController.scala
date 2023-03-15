865702ec7483fc4a61ce5618a3b86a1c68948c2a
ndrew2 <andgri200@gmail.com>
2023-03-15 09:29:55 +0300

    fixed prototype

--- old/gameset/app/controllers/GamesController.scala
+++ old/gameset/app/controllers/GamesController.scala
@@ -9,9 +9,12 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import java.nio.file.Paths
 import javax.inject._
 import models._
+import scala.sys.process._
+import java.io.File
+import compiler.Compiler
 
 import java.io.File
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -24,7 +27,8 @@ class GamesController @Inject() (val controllerComponents: ControllerComponents,
   val games = TableQuery[GamesTable]
   val SRC_DIR = "../data/games"
   def index() = Action.async { req =>
-
+    val schema = games.schema ++ TableQuery[CompilationReportsTable].schema ++ TableQuery[MatchReportsTable].schema ++ TableQuery[SolutionsTable].schema ++ TableQuery[ProgramsTable].schema
+    db.run(schema.createIfNotExists)
     db.run(games.result)
       .map { allGames =>
         Ok(views.html.games.index(allGames))
@@ -33,6 +37,56 @@ class GamesController @Inject() (val controllerComponents: ControllerComponents,
 
   def addGet() = Action {
     Ok(views.html.games.add())
+  }
+
+  def run(id: Long, solutionID1: Long, solutionID2: Long) = Action.async { req =>
+    val game = db.run(games.filter(_.id === id).result).map(_.head)
+    val solution1 = db.run(TableQuery[SolutionsTable].filter(_.id === solutionID1).result).map(_.head)
+    val solution2 = db.run(TableQuery[SolutionsTable].filter(_.id === solutionID2).result).map(_.head)
+
+    for {
+      g <- game
+      s1 <- solution1
+      s2 <- solution2
+      intZ <- Compiler.compile(g.interactor, Seq("-I", SRC_DIR + "/include"))
+      sol1Z <- Compiler.compile(Solution(s1.id, s1.gameID).path, Seq("-I", SRC_DIR + "/include"))
+      sol2Z <- Compiler.compile(Solution(s2.id, s2.gameID).path, Seq("-I", SRC_DIR + "/include"))
+    } yield {
+      val runId = java.util.UUID.randomUUID.toString
+      val sol1pipe = "/tmp/pipe" + s1.id.toString + runId
+      val sol2pipe = "/tmp/pipe" + s2.id.toString + runId
+      println(sol1pipe)
+      println(sol2pipe)
+      if (new File(sol1pipe ++ "In").exists()) {
+        new File(sol1pipe ++ "In").delete()
+      }
+      if (new File(sol1pipe ++ "Out").exists()) {
+        new File(sol1pipe ++ "Out").delete()
+      }
+      if (new File(sol2pipe ++ "In").exists()) {
+        new File(sol2pipe ++ "In").delete()
+      }
+      if (new File(sol2pipe ++ "Out").exists()) {
+        new File(sol2pipe ++ "Out").delete()
+      }
+      ("mkfifo " ++ sol1pipe ++ "In").!!
+      ("mkfifo " ++ sol1pipe ++ "Out").!!
+      ("mkfifo " ++ sol2pipe ++ "In").!!
+      ("mkfifo " ++ sol2pipe ++ "Out").!!
+      (for {
+        int <- intZ
+        sol1 <- sol1Z
+        sol2 <- sol2Z
+      } yield {
+        val proc1p = Seq(int, sol1pipe ++ "In" ++ ":" ++ sol1pipe ++ "Out", sol2pipe ++ "In" ++ ":" ++ sol2pipe ++ "Out")
+        val sol1p = sol1 #< new File(sol1pipe ++ "In") #> new File(sol1pipe ++ "Out")
+        val sol2p = sol2 #< new File(sol2pipe ++ "In") #> new File(sol2pipe ++ "Out")
+        sol1p.run()
+        sol2p.run()
+        println("run1")
+        Ok(proc1p.!!)
+      }).getOrElse(Ok("Compilation error"))
+    }
   }
 
   def addPost() = Action.async(parse.multipartFormData) { req =>
