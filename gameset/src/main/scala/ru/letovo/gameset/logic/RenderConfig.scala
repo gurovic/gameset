42c001913cc64c974e53c89dcd19a40319bdc54b
Vladimir Gurovits <gurovic@gmail.com>
2023-04-25 12:35:38 +0300

    Revert "add Solution and Video models"

--- gameset/src/main/scala/ru/letovo/gameset/logic/RenderConfig.scala
+++ gameset/src/main/scala/ru/letovo/gameset/logic/RenderConfig.scala
@@ -1,7 +1,5 @@
 package ru.letovo.gameset.logic
 
-import ru.letovo.gameset.logic.VideoCodec.VideoCodec
+import VideoCodec.VideoCodec
 
-case class RenderConfig(viewport: ViewportSize, bitrate: Float /*mbps*/ , codec: VideoCodec, compression: Int) {
-  require(compression >= 0 && compression <= 100, "Compression value must be between 0 and 100")
-}
+case class RenderConfig(viewport: ViewportSize, bitrate: Float /*mbps*/ , codec: VideoCodec, compression: CompressionValue)
