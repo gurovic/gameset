6aa83113c2ecea3f1344fdfdf687a511383eb04d
ndrew2 <andgri200@gmail.com>
2023-04-25 23:41:06 +0300

    merge

---
+++ gameset/src/main/scala/ru/letovo/gameset/web/models/Tournament.scala
@@ -0,0 +1,30 @@
+package ru.letovo.gameset.web.models
+
+import slick.jdbc.PostgresProfile.api._
+import slick.lifted.Tag
+import models._
+import ru.letovo.gameset.logic.{TournamentFinishedObserver, TournamentSystem, TournamentSystemRegistry}
+import ru.letovo.gameset.web.models.{User, UsersTable}
+
+case class Tournament(id: Long,
+                      name: String,
+                      gameID: Long,
+                      authorID: Long,
+                      tournamentSystemID: Long,
+                     )
+
+
+class TournamentTable(tag: Tag) extends Table[Tournament](tag, "tournaments") {
+  def id = column[Long]("id", O.PrimaryKey, O.AutoInc)
+
+  def name = column[String]("name")
+
+  def gameID = column[Long]("gameID")
+  def game = foreignKey("GAME", gameID, TableQuery[GamesTable])(_.id)
+
+  def authorID = column[Long]("authorid")
+  def author = foreignKey("AUTHOR", authorID, TableQuery[UsersTable])(_.id)
+  def tournamentSystemID = column[Long]("tournamentSystemID")
+
+  def * = (id, name, gameID, authorID, tournamentSystemID) <> (Tournament.tupled, Tournament.unapply)
+}
