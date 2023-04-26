862d51b5fd9c559edd756ffe0aeede65187e27b0
seawind777 <SA_gaponov@bk.ru>
2023-04-26 08:49:55 +0300

    Пофиксил MR-ORM

--- gameset/src/main/scala/ru/letovo/gameset/web/models/MatchReport.scala
+++ gameset/src/main/scala/ru/letovo/gameset/web/models/MatchReport.scala
@@ -1,11 +1,7 @@
 package ru.letovo.gameset.web.models
 
-import play.api.db.slick.DatabaseConfigProvider
-import play.api.libs.json.Json
-import slick.jdbc.JdbcProfile
 
-import javax.inject.{Inject, Singleton}
-import scala.concurrent.{ExecutionContext, Future}
+import models._
 import slick.jdbc.PostgresProfile.api._
 import slick.lifted.Tag
 
@@ -15,21 +11,12 @@ case class MatchReport(matchId: Long,
                        ErrorCode: Boolean,
                        ErrorSolutions: List[Int])
 
-object MatchReport{
-  implicit val reportformat = Json.format[MatchReport]
-}
+
 
 class MatchReportsTable(tag: Tag) extends Table[MatchReport](tag, "reports") {
 
-  /**
-   * This is the tables default "projection".
-   *
-   * It defines how the columns are converted to and from the Solution object.
-   *
-   * In this case, we are simply passing the ID and gameID parameters to the Solution case classes
-   * apply and unapply methods.
-   */
-  def * = (matchId, solutionScores, PlacesOnly, ErrorCode, ErrorSolutions) <> ((MatchReport.apply _).tupled, MatchReport.unapply)
+
+  def * = (matchId, solutionScores, PlacesOnly, ErrorCode, ErrorSolutions) <> (MatchReport.tupled, MatchReport.unapply)
 
   /** The ID column, which is the primary key, and auto incremented */
   def matchId = column[Long]("matchId")
@@ -42,55 +29,3 @@ class MatchReportsTable(tag: Tag) extends Table[MatchReport](tag, "reports") {
 
 }
 
-/**
- * A repository for solutions.
- *
- * @param dbConfigProvider The Play db config provider. Play will inject this for you.
- */
-@Singleton
-class MatchReportsRepository @Inject()(dbConfigProvider: DatabaseConfigProvider)(implicit ec: ExecutionContext) {
-  // We want the JdbcProfile for this provider
-  private val dbConfig = dbConfigProvider.get[JdbcProfile]
-
-  // These imports are important, the first one brings db into scope, which will let you do the actual db operations.
-  // The second one brings the Slick DSL into scope, which lets you define the table and other queries.
-
-  import dbConfig._
-  import profile.api._
-
-  /**
-   * The starting point for all queries on the solutions table.
-   */
-  private val report = TableQuery[MatchReportsTable]
-
-  /**
-   * Create a solution with the given gameID.
-   *
-   * This is an asynchronous operation, it will return a future of the created solution, which can be used to obtain the
-   * id for that solution.
-   */
-  def create(matchId: Long, solutionScores: Map[Long, Int], PlacesOnly: Boolean, ErrorCode: Boolean, ErrorSolutions: List[Int]): Future[MatchReport] = db.run {
-    // We create a projection of just the gameID column, since we're not inserting a value for the id column
-    (report.map(p => (p.matchId)),
-      report.map(p => (p.solutionScores)),
-      report.map(p=> (p.PlacesOnly)),
-      report.map(p=> (p.ErrorCode)),
-      report.map(p=> (p.ErrorSolutions))
-      )+= matchId
-  }
-
-  /**
-   * List all the people in the database.
-   */
-  def list(): Future[Seq[MatchReport]] = db.run {
-    report.result
-  }
-
-  def getMatchReport(matchId: Long) = db.run {
-    report.filter(_.matchId === matchId).result
-  }
-
-  /**
-   * Here we define the table. It will have a name of people
-   */
-}
