02ed9a8f0eb82cf64ba37073c1ecc6271f33fc0f
KirillMaglysh <kirillmaglys@gmail.com>
2023-05-03 09:15:46 +0300

    Renderer (TMP COMMIT)

---
+++ gameset/src/main/scala/ru/letovo/gameset/logic/MultiPictureRenderer.scala
@@ -0,0 +1,21 @@
+package ru.letovo.gameset.logic
+
+import ru.letovo.gameset.web.models.Video
+
+class MultiPictureRenderer extends Renderer {
+  override def startRendering(matchReport: MatchReport, renderConfig: RenderConfig, callback: Video => Unit): Unit = {
+    InvokerPool.addToQueue(new InvokerRequest(
+      Array(prepareInvoker()),
+      encode,
+      None
+    ))
+  }
+
+  def prepareInvoker(matchReport: MatchReport): Invoker = {
+    val invoker = new Invoker()
+  }
+
+  def encode(): Unit = {
+    callback(new Encoder().encode(, matchReport.matchId, renderConfig))
+  }
+}
