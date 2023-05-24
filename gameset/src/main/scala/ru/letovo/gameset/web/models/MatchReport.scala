3ae4801d5fcab970e9708aab5f4d4ecbbd901368
seawind777 <SA_gaponov@bk.ru>
2023-05-24 09:28:07 +0300

    Match Report Orm

--- gameset/src/main/scala/ru/letovo/gameset/web/models/MatchReport.scala
+++ gameset/src/main/scala/ru/letovo/gameset/web/models/MatchReport.scala
@@ -5,33 +5,55 @@ import models._
 import slick.jdbc.PostgresProfile.api._
 import slick.lifted.Tag
 
+
+
 case class MatchReport(matchId: Long,
-                       solutionScores: Map[Long, Int],
                        PlacesOnly: Boolean,
                        errorCode: Boolean,
-                       errorSolution: Map[Long,String],
                        interactorError: Boolean,
                        interactorLog: String)
 
+case class SolutionScore(Id: Long,
+                         solutionId: Long,
+                         solutionScore: Int,
+                         matchId: Long)
 
 
 class MatchReportsTable(tag: Tag) extends Table[MatchReport](tag, "reports") {
 
-
-  def * = (matchId, solutionScores, placesOnly, errorCode, errorSolution) <> (MatchReport.tupled, MatchReport.unapply)
-
   /** The ID column, which is the primary key, and auto incremented */
-  def matchId = column[Long]("matchid")
+  def matchId = column[Long]("matchid", O.PrimaryKey, O.AutoInc)
 
-  def solutionScores =  column[Map[Long, Int]]("solutionscores")
   def placesOnly = column[Boolean]("placesonly")
 
   def errorCode = column[Boolean]("errorcode")
-  def errorSolution = column[Map[Long, String]]("errorsolutions")
+  def errorSolution = column[Boolean]("errorsolutions")
 
   def interactorError = column[Boolean]("interactorerror")
 
   def interactorLog = column[String]("interactorerrorlog")
 
+  def * = (matchId, placesOnly, errorCode, errorSolution, interactorError, interactorLog)
+}
+
+ val reports = TableQuery[MatchReportsTable]
+
+val solutions = TableQuery[SolutionsTable]
+class  SolutionScoresTable(tag: Tag) extends Table[SolutionScore](tag, "matchreportresults") {
+  def Id = column[Long]("Id", O.PrimaryKey, O.AutoInc)
+
+  def solutionId = column[Long]("solutionId")
+
+  def solutionScore = column[Int]("solutionScore")
+
+  def matchId = column[Long]("matchId")
+
+  def * = (Id, solutionId, solutionScore, matchId)
+
+  def aFK = foreignKey("matchId", matchId, reports)(_.matchId, onDelete =
+    ForeignKeyAction.Cascade)
+
+  def bFK = foreignKey("solutionId", solutionId, solutions)(_.id, onDelete =
+    ForeignKeyAction.Cascade)
 }
 
